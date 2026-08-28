52751bf110b933b0cf2ae2d11485d10d5a2f427d
realthash <thashmikarathnayake11@gmail.com>
2026-08-28 21:37:49 +0530

    feat: add Toaster notification component to app

--- inventory-management/src/main.jsx
+++ inventory-management/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import App from './App.jsx'
 import './styles/global.css'
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster position="top-right" />
     </BrowserRouter>
   </StrictMode>,
 )
